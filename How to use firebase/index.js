0f62b2c58df5ebd87661ba9756d4f1db1edb3e73
KienDao <daokien2303@gmail.com>
2025-03-22 16:27:10 +0700

    add new data to filestore

--- How to use firebase/index.js
+++ How to use firebase/index.js
@@ -3,23 +3,39 @@ function loadProducts() {
     productsContainer.innerHTML = ' ';
 
     db.collection("products").get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            const product = doc.data();
-            const productElement = document.createElement("div")
-            productElement.innerHTML = `
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const product = doc.data();
+                const productElement = document.createElement("div")
+                productElement.innerHTML = `
             <img src=${product.image}>
             <p>Tên sản phẩm: ${product.name}</p>
             <p>Gía tiền: ${product.price}</p>`
 
-            productsContainer.appendChild(productElement);
-        });
+                productsContainer.appendChild(productElement);
+            });
 
-        
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });
+
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
 }
 
 window.onload = loadProducts;
+
+
+function addProduct(name, price) {
+    // Add a new document with a generated id.
+    db.collection("products").add({
+        name: name,
+        price: price
+    })
+        .then(() => {
+            console.log("Add new product successfully");
+            loadProducts();
+        })
+        .catch((error) => {
+            console.error("Error adding document: ", error);
+        });
+}
